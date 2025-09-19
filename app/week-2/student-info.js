f013aaf9826f6a4d009fb4945243e655f0d5f5cf
EnoEkpenyong <EnoAttah.Ekpenyong@edu.sait.ca>
2025-09-19 01:03:05 -0600

    cprg306-assignments

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>EnoEkpenyong</p>
+      <Link href="https://github.com/EnoEkpenyong/cprg306-assignments">
+        https://github.com/EnoEkpenyong/cprg306-assignments
+      </Link>
+    </div>
+  );
+}
